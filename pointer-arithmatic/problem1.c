fc9f3d1d3c69e27cf31a7f87edf817125fa5f0e7
Wagner, J <35028309+dota247@users.noreply.github.com>
2023-02-24 19:58:51 -0800

    Update problem1.c

--- pointer-arithmatic/problem1.c
+++ pointer-arithmatic/problem1.c
@@ -1,3 +1,21 @@
-#import<stdio.h>
+int main(){
 
-printf("test");
+
+    int x = 10;
+    int y = 15;
+
+    int *temp1 = &x;
+    int *temp2 = &y;
+
+
+    printf("x : %d : %d \n", x,&x);
+    printf("y : %d : %d \n", y,&y);
+
+    y = *temp1;
+    x = *temp2;
+
+    printf("x : %d : %d\n", x,&x);
+    printf("y : %d : %d\n", y,&y);
+
+    return 0;
+}
