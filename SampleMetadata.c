536b16958db314608b987803bdb545d8e006a170
JW <35028309+potionlibrary@users.noreply.github.com>
2024-11-11 12:06:53 -0800

    Update and rename Metadata.c to SampleMetadata.c

---
+++ SampleMetadata.c
@@ -0,0 +1,11 @@
+/*
+ * +-----------------------------------------------+
+ * | Module   :  Network Connection Manager        |
+ * | Author   :  Alex Brown                        |
+ * | Date     :  August 15, 2023                   |
+ * | Version  :  2.0                               |
+ * | Purpose  :  Establish and manage network      |
+ * |          ... connections with remote servers.  |
+ * +-----------------------------------------------+
+ */
+
